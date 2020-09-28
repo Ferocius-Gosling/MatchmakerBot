0759a86f8c27262d18bd8be141836a1ab8d54015
Dimitry <Kholod.ural@gmail.com>
2020-09-28 14:00:48 +0500

    added enum for user states and basic processing for it in user

---
+++ MatchmakerBot/src/com/company/States.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public enum States {
+    START,
+    REG_NAME,
+    REG_AGE,
+    REG_CITY,
+    REG_INFO,
+    MENU,
+    DIALOG
+    //etc
+}
--- MatchmakerBot/src/com/company/User.java
+++ MatchmakerBot/src/com/company/User.java
@@ -2,7 +2,23 @@ package com.company;
 
 public class User {
     private int id;
-    public User(int id){
+    private States state;
+    //мб таблица для кон.автомата, дабы можно было прямо по ней выводить на экран доступные пути для юзера
+
+    public User(int id) {
         this.id = id;
+        this.state = States.START;
+
+    }
+
+    public States getCurrentState() {
+        return this.state;
+    }
+
+    public void changeCurrentState(States nextState) {
+        //mb validation
+        this.state = nextState;
     }
 }
+
+
