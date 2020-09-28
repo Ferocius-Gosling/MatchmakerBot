081d7d602dc295fa11fe69c0c311b5aba71dea70
Titov Stepan <titovstepan@mail.ru>
2020-09-28 22:11:55 +0500

    add tests

--- MatchmakerBot/src/test/DialogLogicTest.java
+++ MatchmakerBot/src/test/DialogLogicTest.java
@@ -3,6 +3,7 @@ package test;
 
 import com.company.AnswersStorage;
 import com.company.DialogLogic;
+import com.company.States;
 import com.company.User;
 import org.junit.Assert;
 import org.junit.Test;
@@ -21,6 +22,37 @@ public class DialogLogicTest {
     public void testRegResponse(){
         var string = logic.getResponse(user0, "/reg");
         Assert.assertEquals(string, AnswersStorage.registerNameMessage);
+    }
+
+    @Test
+    public void testRegAge(){
+        var user = new User(1);
+        user.changeCurrentState(States.REG_AGE);
+        var response = logic.getResponse(user, "20");
+        Assert.assertEquals(response, AnswersStorage.regCityMessage);
+    }
+
+    @Test
+    public void testRegAgeIncorrect(){
+        var user = new User(1);
+        user.changeCurrentState(States.REG_AGE);
+        var resp = logic.getResponse(user, "Мне двадцать лет");
+        Assert.assertEquals(resp, AnswersStorage.wrongAgeMessage);
+    }
+
+    @Test
+    public void testFullRegistration(){
+        var user = new User(1);
+        var reg = logic.getResponse(user, "/reg");
+        var name = logic.getResponse(user, "John Smith");
+        var age = logic.getResponse(user, "23");
+        var city = logic.getResponse(user, "New York");
+        var info = logic.getResponse(user, "Something about me");
+        Assert.assertEquals(reg, AnswersStorage.registerNameMessage);
+        Assert.assertEquals(name, AnswersStorage.regAgeMessage);
+        Assert.assertEquals(age, AnswersStorage.regCityMessage);
+        Assert.assertEquals(city, AnswersStorage.regInfoMessage);
+        Assert.assertEquals(info, AnswersStorage.regSuccesfull);
     }
 
     @Test
