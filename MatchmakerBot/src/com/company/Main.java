5a4727bd1d1d3e848bd40286ed21e31f45a68986
Dimitry <Kholod.ural@gmail.com>
2020-11-24 13:25:48 +0500

    all e.print... replaced to log

--- MatchmakerBot/src/com/company/Main.java
+++ MatchmakerBot/src/com/company/Main.java
@@ -9,23 +9,26 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.StringJoiner;
+import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class Main {
+    private static final Logger logger = Logger.getLogger(TelegramBot.class.getName());
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         configureLogger();
         DialogLogic logic = new DialogLogic();
         UserRepository users = new UserRepository();
         try {
             users.loadUsers();
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.log(Level.WARNING, e.getMessage(), e);
         }
         Bot bot = new Bot(users, logic);
         ApiContextInitializer.init();
@@ -33,26 +36,22 @@ public class Main {
         try {
             telegramBotsApi.registerBot(new TelegramBot(bot));
         } catch (TelegramApiRequestException e) {
-            e.printStackTrace();
+            logger.log(Level.WARNING, e.getMessage(), e);
         }
 
     }
 
-    private static void configureLogger() {
-        try {
-            StringJoiner joiner = new StringJoiner(File.separator)
-                    .add("MatchmakerBot")
-                    .add("src")
-                    .add("com")
-                    .add("company")
-                    .add("log.config");
-            var pathToConfig = File.separator + joiner.toString();
-            String currentDir = System.getProperty("user.dir");
-            LogManager.getLogManager().readConfiguration(
-                    new FileInputStream(
-                            currentDir + pathToConfig));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private static void configureLogger() throws IOException {
+        StringJoiner joiner = new StringJoiner(File.separator)
+                .add("MatchmakerBot")
+                .add("src")
+                .add("com")
+                .add("company")
+                .add("log.config");
+        var pathToConfig = File.separator + joiner.toString();
+        String currentDir = System.getProperty("user.dir");
+        LogManager.getLogManager().readConfiguration(
+                new FileInputStream(
+                        currentDir + pathToConfig));
     }
 }
